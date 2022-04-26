45d497d313a0af965254bbfbf7d6f1809ec8c0d9
rakhi <rakhiprajapati845@gmail.com>
2022-04-26 22:12:23 +0530

    add function for word and letter count

--- js/app2.js
+++ js/app2.js
@@ -1,12 +1,21 @@
 let preview = document.querySelector('#preview');
 let input = document.querySelector('#inputText');
+let forWordCount = document.querySelector('.word_count');
+let forLetterCount = document.querySelector('.letter_count');
+
 let allAlign = document.querySelector('.align');
-allAlign.addEventListener('click',textAlign)
+
+//all EventListener is here
+allAlign.addEventListener('click',textAlign);
+input.addEventListener('keyup',function(e){
+    wordAndLetterCounter();
+})
 
 function getInputText(){
     preview.innerHTML=input.value;
 }
 
+//text alignment function
 function textAlign(e){
  item = e.target;
  console.log(item.classList[0]);
@@ -30,9 +39,7 @@ function textAlign(e){
     preview.style.textAlign = "justify";
     console.log('align-justify');
  }
- 
 }
-
 
 function update() {
     var forFontFamily = document.getElementById('font-select');
@@ -41,10 +48,10 @@ function update() {
     var forFontSize = document.getElementById('font-size');
     var fontSizeOption = forFontSize.options[forFontSize.selectedIndex];
     var color = document.querySelector('#color');
-      //console.log(color.value);
-
-    // document.getElementById('value').value = fontFamilyOption.value;
-    // document.getElementById('text').value = fontFamilyOption.text;
+            
+     //console.log(color.value);
+     // document.getElementById('value').value = fontFamilyOption.value;
+     // document.getElementById('text').value = fontFamilyOption.text;
      // input.value = input.value.toUpperCase();
 
     preview.style.color = color.value;
@@ -53,6 +60,26 @@ function update() {
     input.style.fontFamily  =`${fontFamilyOption.text}`;
     preview.style.fontSize  = `${fontSizeOption.text}px`;
     input.style.fontSize  =`${fontSizeOption.text}px`;
+}
 
-    
+//function for word counter and letter counter
+
+function wordAndLetterCounter(){
+    let getInputText = input.value;
+    let wordCount =0;
+    let letterCount = 0;
+  for(let i =0;i<getInputText.length;i++){
+
+         if(getInputText.charAt(i) === ' '){
+           //console.log(getInputText.charAt(i));
+           wordCount++;
+         }
+         if(getInputText.charAt(i ==='')){
+            letterCount++
+         }
+  }
+  forWordCount.innerHTML = `Word Count Here:${wordCount}..`;
+  forLetterCount.innerHTML = `Letter Count Here:${letterCount}..`
+  console.log("word count" + wordCount);
+  console.log("letter count" + letterCount);
 }
